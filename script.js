3af073039bcf3cde2b3f9a0f6359bfed3cdaedfe
ATUL WAMAN NAGOSE <44936500+atulnagose1499@users.noreply.github.com>
2023-01-19 22:09:17 +0530

    add oops

---
+++ script.js
@@ -0,0 +1,13 @@
+// OOP is a programming paradigm that emphasizes the use of objects and their interactions to design and write a program. It encourages the use of encapsulation, inheritance, polymorphism, and abstraction to create a more organized, maintainable and efficient codebase.key concepts 6
+
+// Classes: A class can be thought of as a blueprint for creating objects. An example of a class in real life would be a blueprint for building a house. The blueprint defines the layout, number of rooms, and overall design of the house, but it doesn't specify the actual materials or colors that will be used.
+
+// Objects: An object is an instance of a class. An example of an object in real life would be a specific house built according to the blueprint. The house has a specific location, materials, and colors, but it follows the overall design and layout specified in the blueprint.
+
+// Encapsulation: Encapsulation is the process of hiding the internal state and behavior of an object. An example of encapsulation in real life would be a locked safe. The safe has a specific combination that must be entered to open it, but the inner workings of the safe are hidden from the user.
+
+// Inheritance: Inheritance is the process by which one class inherits the properties and methods of another class. An example of inheritance in real life would be a car. A car inherits properties such as wheels, a steering wheel, and an engine from the general class of "vehicle."
+
+// Polymorphism: Polymorphism is the ability of an object to take on multiple forms. An example of polymorphism in real life would be a pen. A pen can be used to write in multiple colors, and it can write on different types of paper, but it still follows the same basic shape and function.
+
+// Abstraction: Abstraction is the process of hiding complex implementation details and providing a simple, easy-to-use interface. An example of abstraction in real life would be a remote control. The remote control has buttons to control various functions of a TV, such as changing the channel, adjusting the volume, and turning the TV off, but the user doesn't need to know the specific electronic or programming details of how the TV works.
