e8c25119c8fda16333883bddc90761fc36d70443
ATUL WAMAN NAGOSE <44936500+atulnagose1499@users.noreply.github.com>
2023-01-20 20:50:53 +0530

    add react notes 1,2

---
+++ 1react.js
@@ -0,0 +1,61 @@
+// React is a JavaScript library for building user interfaces. It allows developers to create reusable UI components and manage the state of their applications in an efficient way. It helps to create maintainable and testable code by breaking down the UI into smaller components. It's primarily used for building complex and dynamic web applications such as single-page applications (SPAs) and progressive web apps (PWAs).
+
+// a Single-Page Application (SPA) is a type of web application that loads a single HTML page and dynamically updates the content as the user interacts with the app
+
+// Progressive Web Apps (PWAs) are web applications that have native app-like features, such as offline functionality and push notifications, and can be accessed through a web browser.
+
+// React is used to build dynamic, efficient, and scalable user interfaces. It allows developers to easily update and render components when the underlying data changes, without having to manually update the entire page. This makes React applications more efficient and less prone to errors. Additionally, React also supports server-side rendering, which can improve the load time of web applications by rendering the initial state of the app on the server.
+
+// A component in React is a reusable piece of code that represents a part of a user interface. It can include a combination of JSX, JavaScript and CSS, and can be rendered to the DOM. Components can be nested inside other components and can accept inputs (props) and maintain their own state. They allow for breaking down a complex UI into smaller, more manageable parts, making the code more readable and easier to maintain.
+
+// a website is a collection of pages that provides information, while a Single-Page Application (SPA) is a type of web application that loads a single HTML page and dynamically updates the content as the user interacts with the app.
+// ------------------------------------------------------------------------------------------------------------
+// While there are other JavaScript frameworks and libraries available for building user interfaces, React has some unique features and advantages that make it a popular choice among developers.
+
+// Reusable components: React allows developers to create reusable UI components, which can be easily shared and reused across different parts of an application. This can save time and improve the maintainability of the codebase.
+
+// Virtual DOM: React uses a virtual DOM, which optimizes updates and rendering by only updating the parts of the actual DOM that have changed, rather than re-rendering the entire page. This improves the performance of large and complex applications.
+
+// Unidirectional data flow: React follows a unidirectional data flow, which makes it easier to understand and manage the flow of data within an application, and also allows for better debugging and error handling.
+
+// JSX: React uses JSX, which is a syntax extension for JavaScript that allows developers to write HTML-like elements in their JavaScript code. This can make it easier to read and understand the codebase, and also allows for better type checking and error handling.
+
+// Popularity and community: React has a large and active community, which means there is a lot of support, tutorials, and resources available for developers who are new to the library. Additionally, because many companies are using React, it can be easier to find React developers for your team.
+
+// Compatibility with other libraries and frameworks: React can be easily integrated with other libraries and frameworks, such as Redux and Webpack, which can further improve the functionality and performance of an application.
+
+// Flexibility: React is not a full-featured framework and provides only the view layer of an application, this allows developers to use React in conjunction with other libraries and frameworks, such as Angular or Vue.js, to build complete web applications.
+
+// Facebook and Instagram uses React: React was developed by Facebook and is used in production on some of the world's most popular websites and apps, like Facebook and Instagram which gives confidence to developers and companies that it is a battle-tested technology.
+
+// Overall, React is a powerful and flexible JavaScript library that can be used to build complex user interfaces more efficiently and effectively than with other frameworks and libraries. It provides a set of features and tools that can save developers time and effort and make the development process more manageable and easier to understand.
+// ------------------------------------------------------------------------------------------------------------
+// how create-react-app
+// npx create-react-app my-app
+// -----------------------------------------------------------------------------------------------------------
+// A typical ReactJS project has the following folder structure:
+
+// public: contains the public assets of the project such as the index.html file and any static files.
+// src: contains the source code of the project, including components, images, and styles.
+// node_modules: contains the packages that are installed via npm or yarn.
+// package.json: contains the project's dependencies and scripts.
+// package-lock.json or yarn.lock: contains the exact versions of the packages installed in the project.
+// webpack.config.js: contains the webpack configuration for the project.
+// .babelrc: contains the babel configuration for the project.
+// .eslintrc: contains the eslint configuration for the project.
+// .gitignore: contains the files and directories that should be ignored by git.
+// ------------------------------------------------------------------------------------------------------------
+// Inside the src folder of a ReactJS project, you will typically find the following files and directories:
+
+// index.js: this is the entry point of the application, where the React app is rendered to the DOM. It also contains the code to setup the React app with the DOM.
+// index.html : this file is the template file for the application, which will be processed by webpack and served by the development server.
+// components: this directory contains all the React components of the application, organized into subdirectories by feature or page. Each component is typically defined in a separate file, with the same name as the component.
+// assets: this directory contains any assets that the application needs, such as images, fonts, or icons.
+// styles: this directory contains the styles for the application, organized into subdirectories by feature or page. It might include the css, scss, less or any other preprocessors.
+// routes: this directory contains the routing configuration for the application. It might include the index.js file that exports the routing configuration.
+// redux: this directory contains the state management configuration for the application using redux. It might include the store.js, reducers and actions
+// services: this directory contains the services that the application uses to interact with APIs or other services.
+// Please note that this is just a typical folder structure and the exact structure of your project may vary depending on the project's requirements and the developer's preference.
+
+
+
---
+++ 2jsx.js
@@ -0,0 +1,5 @@
+// Importing allows using contents from another file, whereas exporting makes the file contents eligible for importing
+
+// A fragment in React allows you to group multiple elements together and return them as a single unit, without adding any extra elements to the DOM. It can be useful for keeping your code clean and organized. You can create a fragment by wrapping your elements in a <Fragment> or <> tag.
+
+// Strict mode is a feature in React that helps you find and fix potential problems in your code by adding additional checks and warnings. It can be enabled by wrapping a component in a <StrictMode> tag.
